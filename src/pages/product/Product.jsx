b150bb063060b55be124f8c8d6b5c4bbb0b4f2cd
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-27 22:41:22 -0300

    :sparkles:  feat: added ProductOvervie component to product

---
+++ src/pages/product/Product.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Nav } from "../../components/nav/Nav";
+import { ProductOverview } from "../../components/ProductOverview/ProductOverview";
+ 
+export const Product = () => {
+  return (
+    <>
+      <Nav />
+      <ProductOverview/>
+    </>
+  );
+};
