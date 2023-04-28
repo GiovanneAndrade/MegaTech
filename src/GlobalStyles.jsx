c59b3b66a5a874a246a3c0c45a242e9c5f273158
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-27 22:36:11 -0300

    :lipstick: chore: Add global styles

---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+ 
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    
+    font-family: sans-serif;
+    font-family: 'Roboto', sans-serif;
+    text-decoration: none;
+
+ background:#eeeaea;
+  }
+
+`;
+
+export default GlobalStyles;
