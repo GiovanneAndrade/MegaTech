c826ce0945a7453de2414530992d8de06e131ee5
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-01 19:48:24 -0300

    :sparkles:  feat: add  component stepper e styles

---
+++ src/components/stepper/StepperStyles.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StepperContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+export const Amount = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  width: 100%;
+  padding: 10px;
+  p {
+    font-size: 20px;
+  }
+`;
