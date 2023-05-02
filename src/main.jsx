b8050150e695ff7f5878954edb603e2430067fdb
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-02 19:56:04 -0300

    :sparkles:  feat: add contexts at main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
- 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AppProvider } from "./contexts/AppContext";
+import { CartProvider } from "./contexts/CartContext";
+import { FavoritesProvider } from "./contexts/FavoritesContext";
+import { OrderProvider } from "./contexts/OrderContext";
+import { PaymentProvider } from "./contexts/PaymentContext";
+import { ProductProvider } from "./contexts/ProductContext";
+import { UserProvider } from "./contexts/UserContext";
+import { CategoriesProvider } from "./contexts/Categories";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppProvider>
+      <CartProvider>
+        <FavoritesProvider>
+          <OrderProvider>
+            <PaymentProvider>
+              <ProductProvider>
+                <UserProvider>
+                  <CategoriesProvider>
+                    <App />
+                  </CategoriesProvider>
+                </UserProvider>
+              </ProductProvider>
+            </PaymentProvider>
+          </OrderProvider>
+        </FavoritesProvider>
+      </CartProvider>
+    </AppProvider>
+  </React.StrictMode>
+);
