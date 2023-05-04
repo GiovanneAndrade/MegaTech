41ae5bdd06eda70cc798a1a0effa40c7fe33864e
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-04 20:47:00 -0300

    :sparkles:  feat: leaving cart dynamic

--- src/hooks/UseCart.jsx
+++ src/hooks/UseCart.jsx
@@ -1,44 +1,29 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { CartContext } from '../contexts/CartContext';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../contexts/CartContext";
 
 function useShoppingCart() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      name: "Product A",
-      price: 10,
-      quantity: 1,
-      imageUrl: "https://imgs.casasbahia.com.br/55011374/1xg.jpg?imwidth=292",
-    },
-    {
-      id: 2,
-      name: "Product B",
-      price: 20,
-      quantity: 1,
-      imageUrl: "https://imgs.casasbahia.com.br/55011374/1xg.jpg?imwidth=292",
-    },
-    {
-      id: 3,
-      name: "Product C",
-      price: 15,
-      quantity: 1,
-      imageUrl: "https://imgs.casasbahia.com.br/55011374/1xg.jpg?imwidth=292",
-    },
-  ]);
+  const existingCart = localStorage.getItem("megaTechCart");
+  let initialItemsState = [];
+
+  if (existingCart) {
+    initialItemsState = JSON.parse(existingCart);
+  }
+
+  const [items, setItems] = useState(initialItemsState);
 
   const handleDeleteItem = (id) => {
     setItems(items.filter((item) => item.id !== id));
   };
 
   const subtotal = items.reduce(
-    (acc, item) => acc + item.price * item.quantity,
+    (acc, item) => acc + Number(item.price) * item.quantity,
     0
   );
 
   const handleAddItem = (id) => {
     const newItems = items.map((item) =>
-      item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+      item.id === id ? { ...item, quantity: Number(item.quantity) + 1 } : item
     );
     setItems(newItems);
   };
@@ -46,7 +31,7 @@ function useShoppingCart() {
   const handleRemoveItem = (id) => {
     const newItems = items.map((item) =>
       item.id === id
-        ? { ...item, quantity: Math.max(item.quantity - 1, 0) }
+        ? { ...item, quantity: Math.max(Number(item.quantity) - 1, 0) }
         : item
     );
     setItems(newItems);
@@ -67,13 +52,44 @@ export const useAddCart = () => {
   const { numberCart, setNumberCart } = React.useContext(CartContext);
   const navigate = useNavigate();
 
-  function handleAddCart(){
-    setNumberCart(numberCart + 1)
+  function handleAddCart(productOverview) {
+    setNumberCart(Number(numberCart) + 1);
+  
+    const existingCart = localStorage.getItem("megaTechCart");
+    if (existingCart) {
+      const cartArray = JSON.parse(existingCart);
+      const productIndex = cartArray.findIndex(item => item.id === productOverview.id);
+      if (productIndex >= 0) {
+        cartArray[productIndex].quantity += productOverview.quantity;
+      } else {
+        cartArray.push(productOverview);
+      }
+      localStorage.setItem("megaTechCart", JSON.stringify(cartArray));
+    } else {
+      const cartArray = [productOverview];
+      localStorage.setItem("megaTechCart", JSON.stringify(cartArray));
+    }
+  }
+  
+  
+
+  function showShopping(typeButton, productOverview) {
+    const existingCart = localStorage.getItem("megaTechCart");
+    if (existingCart) {
+      const cartArray = JSON.parse(existingCart);
+      const productIndex = cartArray.findIndex(item => item.id === productOverview.id);
+      if (productIndex >= 0) {
+        cartArray[productIndex].quantity += productOverview.quantity;
+      } else {
+        cartArray.push(productOverview);
+      }
+      localStorage.setItem("megaTechCart", JSON.stringify(cartArray));
+    } else {
+      const cartArray = [productOverview];
+      localStorage.setItem("megaTechCart", JSON.stringify(cartArray));
+    }
+    navigate(typeButton);
   }
 
-  function showShopping (typeButton){
-    navigate(typeButton)
-  }
-
-  return {  handleAddCart, showShopping };
+  return { handleAddCart, showShopping };
 };
