04ecebaee6a3b9c8bc3353bef6168c172af72c1a
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-04 20:52:48 -0300

    :sparkles:  feat: adjusting the number of items in the cart

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const CartContext = React.createContext({});
  
 
 export const CartProvider = (props) => {
-  const [ numberCart, setNumberCart ] = useState(0)
+  const [numberCart, setNumberCart] = useState(0);
+
+  useEffect(() => {
+    const existingCart = localStorage.getItem("megaTechCart");
+    if (existingCart) {
+      const cartArray = JSON.parse(existingCart);
+      setNumberCart(cartArray.length);
+    }
+  }, []);
+
   return (
     <CartContext.Provider value={{ numberCart, setNumberCart }}>
       {props.children}
     </CartContext.Provider>
   );
 };
+
