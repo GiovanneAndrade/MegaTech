d7ff58962fd1abe6f3340541ecc9f08b1fa84432
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-16 19:27:40 -0300

    :sparkles:	fix: componet nav dinamic

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -14,13 +14,20 @@ import {
 import { Cart } from "../cart/Cart";
 import { useNavigate } from "react-router-dom";
 import { Categories } from "../../pages/categories/Categories";
+import { AppContext } from "../../contexts/AppContext";
+import { CategoriesContext } from "../../contexts/Categories";
 export const Nav = () => {
+  const { showCategory, setShowCategory } = React.useContext(CategoriesContext);
   const navigate = useNavigate();
+  function home(){
+    navigate("/")
+    setShowCategory(true)
+  }
   return (
     <NavContainer>
       <MenuList>
         <MenuLeft>
-          <Logo onClick={() => navigate("/")}>My App</Logo>
+          <Logo onClick={home}>My App</Logo>
           <div>CEP 666000555</div>
         </MenuLeft>
         <MenuCentral>
@@ -28,8 +35,8 @@ export const Nav = () => {
           
           <ul>
             <li><Categories/></li>
-            <li >Ofertas</li>
-            <li>Historico</li>
+            <li onClick={() => navigate("/offers")}>Ofertas</li>
+            <li onClick={() => navigate("/historic")}>Historico</li>
             <li onClick={() => navigate("/favorites")}>Favoritos</li>
             <li onClick={() => navigate("/contact")}>Contato</li>
             <li onClick={() => navigate("/orders")} >meus pedidos</li>
