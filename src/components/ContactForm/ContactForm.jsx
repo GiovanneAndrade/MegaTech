1b8822116f7d0a939d4c3f947c15210d6eccbef2
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-18 19:18:50 -0300

    :sparkles:	feat: static query form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -11,31 +11,32 @@ import {
 } from "@mui/material";
 
 export const ContactForm = () => {
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [message, setMessage] = React.useState("");
-  const [nameError, setNameError] = React.useState(false);
-  const [emailError, setEmailError] = React.useState(false);
-  const [messageError, setMessageError] = React.useState(false);
-  const [formSubmitted, setFormSubmitted] = React.useState(false);
-  const [selectedOrder, setSelectedOrder] = React.useState("");
-  const [selectedOrderError, setSelectedOrderError] = React.useState(false);
-  const [selectedReasonError, setSelectedReasonError] = React.useState(false);
-  const [selectedReason, setSelectedReason] = React.useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (!name || !email || !message || !selectedOrder || !selectedReason) {
-      if (!name) setNameError(true);
-      if (!email) setEmailError(true);
-      if (!message) setMessageError(true);
-      if (!selectedOrder) setSelectedOrderError(true);
-      if (!selectedReason) setSelectedReasonError(true);
-      return;
-    }
-
-    setFormSubmitted(true);
-  };
+  const {
+    handleSubmit,
+    name,
+    setName,
+    nameError,
+    setNameError,
+    email,
+    setEmail,
+    emailError,
+    setEmailError,
+    message,
+    setMessage,
+    messageError,
+    setMessageError,
+    formSubmitted,
+    selectedOrder,
+    setSelectedOrder,
+    selectedOrderError,
+    setSelectedOrderError,
+    selectedReason,
+    setSelectedReason,
+    selectedReasonError,
+    setSelectedReasonError
+  } = useHandleSubmit();
+  
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -130,6 +131,7 @@ export const ContactForm = () => {
   );
 };
 import styled from "styled-components";
+import { useHandleSubmit } from "../../hooks/UseContact";
 
 export const Form = styled.form`
   width: 880px;
