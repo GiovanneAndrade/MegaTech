10bcea05697deaa18934dbacc555bb0c1944ea68
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-19 19:45:05 -0300

    :sparkles:	fix: category dynamic hook

--- src/hooks/UseCategory.jsx
+++ src/hooks/UseCategory.jsx
@@ -3,18 +3,25 @@ import { CategoriesContext } from "../contexts/Categories";
 
 // Hook customizado para gerenciar o estado das categorias selecionadas
 export function useCategorySelection(initialCategory) {
+  const { showCategory, setShowCategory , isCategory, setIsCategory} = React.useContext(CategoriesContext);
+  
   const [selectedCategory, setSelectedCategory] =
     React.useState(initialCategory);
 
   const handleCategoryClick = (category) => {
-    setSelectedCategory(category);
+ 
+    setSelectedCategory(category?.name);
+    setIsCategory(category)
+    if(showCategory){
+      setShowCategory(false)
+    }
   };
 
   return [selectedCategory, handleCategoryClick];
 }
 
 export const usePopover = () => {
-  const { anchorEl, setAnchorEl } = React.useContext(CategoriesContext);
+  const { anchorEl, setAnchorEl, category } = React.useContext(CategoriesContext);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
