a24393df26501011a5ac93da6aa91612d2ee692f
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-22 18:38:44 -0300

    :sparkles:	fix: context orders

--- src/contexts/OrderContext.jsx
+++ src/contexts/OrderContext.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getRequests } from "../services/Services";
 
 export const OrderContext = React.createContext({});
-const Order = "Order";
 export const OrderProvider = (props) => {
+  const [isChecked, setIsChecked] = React.useState(false);
+  const [isTotal, setIsTotal] = React.useState();
+  const [finalOrder, setFinalOrder] = React.useState();
+  const [errorOrder, setErrorOrder] = React.useState(true);
+  const [order, setOrder] = React.useState();
+ const [ newOrder, setNewOrder ] = React.useState(false);
+  useEffect(() => {
+    const orders = getRequests();
+    orders
+      .then((response) => {
+        setOrder(response?.data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }, [newOrder, order]);
+
   return (
-    <OrderContext.Provider value={{ Order }}>
+    <OrderContext.Provider
+      value={{
+        isChecked,
+        setIsChecked,
+        isTotal,
+        setIsTotal,
+        finalOrder,
+        setFinalOrder,
+        errorOrder,
+        setErrorOrder,
+        order,
+        setOrder,
+        setNewOrder
+      }}
+    >
       {props.children}
     </OrderContext.Provider>
   );
