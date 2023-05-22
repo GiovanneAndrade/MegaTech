429a784ce5926cf7070885733d791ab8c519ac08
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-22 18:39:32 -0300

    :sparkles:	fix: context categories

--- src/contexts/Categories.jsx
+++ src/contexts/Categories.jsx
@@ -1,13 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { getCategories } from "../services/Services";
 
 export const CategoriesContext = React.createContext({});
 
- 
 export const CategoriesProvider = (props) => {
   const [anchorEl, setAnchorEl] = useState(null);
- 
+  const [category, setCategory] = useState([]);
+  const [showCategory, setShowCategory] = useState(true);
+  const [isCategory, setIsCategory] = useState([]);
+
+  useEffect(() => {
+    const categories = getCategories();
+    categories
+      .then((response) => {
+        setCategory(response?.data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }, []);
+
   return (
-    <CategoriesContext.Provider value={{ anchorEl, setAnchorEl}}>
+    <CategoriesContext.Provider
+      value={{
+        anchorEl,
+        setAnchorEl,
+        category,
+        setCategory,
+        showCategory,
+        setShowCategory,
+        isCategory,
+        setIsCategory,
+      }}
+    >
       {props.children}
     </CategoriesContext.Provider>
   );
