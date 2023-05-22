fac91616ad4404e8ed550e4fe20482955b9d14b4
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-22 18:36:51 -0300

    :sparkles:	feat: page component

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,91 +1,32 @@
-import { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  List,
-  ListItem,
-  ListItemText,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-} from "@material-ui/core";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import React, { useState } from "react";
+import { List } from "@material-ui/core";
 import { Nav } from "../../components/nav/Nav";
-
-const useStyles = makeStyles(() => ({
-  root: {
-    width: "100%",
-    maxWidth: 360,
-    backgroundColor: "#f5f5f5",
-    margin: "auto",
-  },
-}));
+import styled from "styled-components";
+import { ContainerHome } from "../home/Home";
+import { OrderContext } from "../../contexts/OrderContext";
+import { AccordionOders } from "../../components/accordionOders/AccordionOders";
 
 function Orders() {
-  const classes = useStyles();
-  const { expandedOrderId, handleOrderClick } = useExpandedOrderId();
+  const { order } = React.useContext(OrderContext);
+  const products = order?.map((product) => product?.products);
+ 
 
   return (
     <>
       <Nav />
       <ContainerHome>
-        <NewList component="nav">
-          <Accordion expanded={expandedOrderId === "order1"}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              onClick={() => handleOrderClick("order1")}
-            >
-              <ListItemText primary="Order #1" secondary="Status: Delivered" />
-            </AccordionSummary>
-            <AccordionDetails>
-              <div>
-                <p>Product name: Product A</p>
-                <p>Date of purchase: January 1, 2023</p>
-                <p>Delivery address: 123 Main St.</p>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-
-          <Accordion expanded={expandedOrderId === "order2"}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              onClick={() => handleOrderClick("order2")}
-            >
-              <ListItemText primary="Order #2" secondary="Status: In transit" />
-            </AccordionSummary>
-            <AccordionDetails>
-              <div>
-                <p>Product name: Product B</p>
-                <p>Date of purchase: February 15, 2023</p>
-                <p>Delivery address: 456 Elm St.</p>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-
-          <Accordion expanded={expandedOrderId === "order3"}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              onClick={() => handleOrderClick("order3")}
-            >
-              <ListItemText primary="Order #3" secondary="Status: Pending" />
-            </AccordionSummary>
-            <AccordionDetails>
-              <div>
-                <p>Product name: Product C</p>
-                <p>Date of purchase: March 31, 2023</p>
-                <p>Delivery address: 789 Oak St.</p>
-              </div>
-            </AccordionDetails>
-          </Accordion>
-        </NewList>
+        {
+          order?.map(item =>(
+            <AccordionOders orders={item}/>
+          ))
+        }
+      
       </ContainerHome>
     </>
   );
 }
 
 export default Orders;
-import styled from "styled-components";
-import { ContainerHome } from "../home/Home";
-import { useExpandedOrderId } from "../../hooks/UseOrders";
 
 export const NewList = styled(List)`
   width: 800px;
