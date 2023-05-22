d9760fb0bd447ca7080b7e799e4313de7f8e3332
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-22 18:39:12 -0300

    :sparkles:	fix: context favorites

--- src/contexts/FavoritesContext.jsx
+++ src/contexts/FavoritesContext.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getFavorities } from "../services/Services";
 
 export const FavoritesContext = React.createContext({});
-const Favorites = "Favorites";
+ 
 export const FavoritesProvider = (props) => {
+  const [favorities, setFavorities] = useState([]);
+  const [myFavorities, setMyFavorities] = useState();
+ 
+  useEffect(() => {
+    const favorities = getFavorities();
+    favorities
+      .then((response) => {
+        setFavorities(response.data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }, []);
+
   return (
-    <FavoritesContext.Provider value={{ Favorites }}>
+    <FavoritesContext.Provider value={{ favorities, setFavorities, myFavorities, setMyFavorities }}>
       {props.children}
     </FavoritesContext.Provider>
   );
