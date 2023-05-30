80d7d68fd4102358055def81362260958ce05094
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-29 22:40:30 -0300

    :sparkles:	feat: page signin

--- src/pages/signin/Signin.jsx
+++ src/pages/signin/Signin.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import { Nav } from "../../components/nav/Nav";
 import { NewContainerHome } from "../historic/Historic";
 import UserRegistrationForm from "../../components/signup/UserRegistrationForm";
-import FormLogin from "../../components/nav/formSignin/FormLogin";
-
+import FormLogin from "../../components/formSignin/FormLogin";
 export const Signin = () => {
   return (
     <>
       <Nav />
-      <NewContainerHome>
-        <FormLogin/>
-      </NewContainerHome>
+      <FormLogin />
     </>
   );
 };
