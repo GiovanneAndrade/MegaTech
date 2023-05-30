52532552293fd9c8771ad45c689c7517114c82e5
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-29 22:43:46 -0300

    :heavy_minus_sign:  chore: remove duplicate function

--- src/components/address/Address.jsx
+++ src/components/address/Address.jsx
@@ -42,9 +42,7 @@ export function AddAnddress() {
     }));
   }
 
-  function handleBuscardistrict() {
-    buscardistrict(myAnddress.cep);
-  }
+ 
   function handleBuscardistrict() {
     buscardistrict(myAnddress.cep);
   }
