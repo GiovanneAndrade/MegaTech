ef595021ae81c55829c8c5fe69dd66e1bdb9ce90
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-29 22:45:21 -0300

    :lipstick:	feat: changing the icon for lottie gifs

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -5,22 +5,46 @@ import styled from "styled-components";
 import { ContainerHome } from "../home/Home";
 import { OrderContext } from "../../contexts/OrderContext";
 import { AccordionOders } from "../../components/accordionOders/AccordionOders";
-
+import pare from "../../assets/images/pare.gif";
+import { getFromLocalStorage } from "../../localStorage/LocalStorage";
+import { NewContainerHome } from "../historic/Historic";
+import Lottie from 'react-lottie';
+import animationData from '../../assets/images/signin.json';
 function Orders() {
   const { order } = React.useContext(OrderContext);
   const products = order?.map((product) => product?.products);
- 
+
+  const myToken = getFromLocalStorage("megaTechAuth");
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+  const lottieStyle = {
+    width: "500px",  
+    height: "500px", 
+  }
 
   return (
     <>
       <Nav />
       <ContainerHome>
-        {
-          order?.map(item =>(
-            <AccordionOders orders={item}/>
-          ))
-        }
-      
+        {myToken ? (
+          <>
+            {order?.map((item) => (
+              <AccordionOders orders={item} />
+            ))}
+          </>
+        ) : (
+          <NewContainerHome>
+            Faça Login Para Prosseguir
+           <Lottie options={defaultOptions} style={lottieStyle} />
+          </NewContainerHome>
+        )}
       </ContainerHome>
     </>
   );
