93a671864b477de4fccb5137e884df85a88d4b01
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-29 22:42:05 -0300

    :sparkles:	feat: page signup

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import { Nav } from "../../components/nav/Nav";
 import { NewContainerHome } from "../historic/Historic";
 import UserRegistrationForm from "../../components/signup/UserRegistrationForm";
-import FormLogin from "../../components/nav/formSignin/FormLogin";
 
 export const Signup = () => {
   return (
     <>
       <Nav />
-      <NewContainerHome>
-       <UserRegistrationForm/> 
-      
-      </NewContainerHome>
+
+      <UserRegistrationForm />
     </>
   );
 };
-
-
