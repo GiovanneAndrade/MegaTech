ed826222b19c0f61a7be9ecbe52e4447caf17a6c
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-30 22:51:28 -0300

    :sparkles:	feat: page user static

---
+++ src/pages/user/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EditProfile from "../../components/EditProfile/EditProfile";
+import { Nav } from "../../components/nav/Nav";
+
+export const User = () => {
+  return (
+    <>
+      <Nav />
+      <EditProfile />
+    </>
+  );
+};
