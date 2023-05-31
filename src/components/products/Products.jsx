d9c5ed19068318dc2a6180ee9710ce77a2a4e350
GiovanneAndrade <vanne.as@hotmail.com>
2023-05-31 18:28:32 -0300

    :sparkles:	feat: componet products dynamics

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,5 +1,9 @@
-import React from "react";
-import { ImageContainer, InformationProducts, ProductsContainer,} from "./ProducsSlyles";
+import React, { useEffect, useState } from "react";
+import {
+  ImageContainer,
+  InformationProducts,
+  ProductsContainer,
+} from "./ProducsSlyles";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import banner from "../../assets/images/ipad-card-40-pro-202108 1.png";
@@ -7,17 +11,82 @@ import { Checkbox } from "@mui/material";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
-export const Products = ({ name, avaliações }) => {
+import { ProductContext } from "../../contexts/ProductContext";
+import { useCreateFavorites } from "../../hooks/UseFavorites";
+import { FavoritesContext } from "../../contexts/FavoritesContext";
+export const Products = ({
+  name,
+  avaliações,
+  image,
+  description,
+  quantity,
+  id,
+  isFavorite,
+}) => {
   const navigate = useNavigate();
+  const [checked, setChecked] = useState(false);
+
   const label = { inputProps: { "aria-label": "Checkbox demo" } };
+  const { productOverview, setProductOverview } =
+    React.useContext(ProductContext);
+  const { myFavorities, setMyFavorities, favorities } =
+    React.useContext(FavoritesContext);
+
+  useEffect(() => {
+    if (favorities?.filter((f) => f.id === id).length > 0 || isFavorite) {
+      setChecked(true);
+    } else {
+      setChecked(false);
+    }
+  }, [favorities, id, isFavorite]);
+
+  function showProduct(name, avaliações, image, description, quantity, id) {
+    setProductOverview({
+      name,
+      price: avaliações,
+      image,
+      description,
+      quantity,
+      id,
+    });
+    navigate("/product");
+  }
+  const { createFavorites, removeFavorites } = useCreateFavorites();
+
+  async function handleCheckboxChange() {
+    setChecked(!checked);
+    if (!checked) {
+      const hasError = await createFavorites(id);
+      setMyFavorities(id);
+    } else {
+      const hasError = await removeFavorites(id);
+    }
+  }
+
   return (
-    <ProductsContainer >
-      <ImageContainer onClick={()=> navigate("/product")}>
-        <img src={banner} />
+    <ProductsContainer>
+      <ImageContainer
+        onClick={() => showProduct(name, avaliações, image, description, 1, id)}
+      >
+        <img src={image} />
       </ImageContainer>
-      <InformationProducts >
-        <h1 onClick={()=> navigate("/product")}> {name}</h1>
-        <p onClick={()=> navigate("/product")}> {avaliações}</p>
+      <InformationProducts>
+        <h1
+          onClick={() =>
+            showProduct(name, avaliações, image, description, 1, id)
+          }
+        >
+          {name}
+        </h1>
+
+        <p
+          onClick={() =>
+            showProduct(name, avaliações, image, description, 1, id)
+          }
+        >
+          {" "}
+          {avaliações}
+        </p>
         <div className="containerProductsLow">
           Avaliações
           <div>
@@ -30,6 +99,8 @@ export const Products = ({ name, avaliações }) => {
                   color: "#fb8500",
                 },
               }}
+              checked={checked}
+              onChange={handleCheckboxChange}
             />
           </div>
         </div>
