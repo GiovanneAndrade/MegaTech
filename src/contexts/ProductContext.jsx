a8176e54c3c49eadca0841a753cc23b36aea3a61
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-02 22:33:39 -0300

    :sparkles:	feat: part of scroll logic in context

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -1,13 +1,22 @@
-import React,{ useState } from "react";
- 
+import React, { useState } from "react";
 
 export const ProductContext = React.createContext({});
 
- 
 export const ProductProvider = (props) => {
- 
+  const [productOverview, setProductOverview] = useState();
+  const [scroll, setScroll] = useState(false);
+  const [inputScroll, setInputScroll] = useState(false);
   return (
-    <ProductContext.Provider>
+    <ProductContext.Provider
+      value={{
+        productOverview,
+        setProductOverview,
+        scroll,
+        setScroll,
+        inputScroll,
+        setInputScroll,
+      }}
+    >
       {props.children}
     </ProductContext.Provider>
   );
