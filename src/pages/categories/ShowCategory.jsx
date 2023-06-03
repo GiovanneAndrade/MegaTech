a3bc907db3ebe2c9ff913fa9ba35900df893ef05
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-02 22:39:15 -0300

    :sparkles:	feat: category page clicked and search

--- src/pages/categories/ShowCategory.jsx
+++ src/pages/categories/ShowCategory.jsx
@@ -1,16 +1,67 @@
-import React from 'react'
-import { CategoriesContext } from '../../contexts/Categories';
-import { Titles } from '../../components/title/Title';
-import { ListProducts } from '../../components/listProducts/ListProducts';
-import { ContainerRightInternal } from '../../components/containerRight/ContainerRightStyles';
-
+import React, { useEffect } from "react";
+import { CategoriesContext } from "../../contexts/Categories";
+import { Titles } from "../../components/title/Title";
+import { ListProducts } from "../../components/listProducts/ListProducts";
+import { ContainerRightInternal } from "../../components/containerRight/ContainerRightStyles";
+import { getFromLocalStorage } from "../../localStorage/LocalStorage";
+import { ProductContext } from "../../contexts/ProductContext";
+import Success from "../../components/lottie/Success";
+import isResult from "../../assets/images/error.json";
+import styled from "styled-components";
 export const ShowCategory = () => {
-    const { showCategory, setShowCategory , isCategory, setIsCategory} = React.useContext(CategoriesContext);
-    console.log(isCategory)
+  const { showCategory, setShowCategory, isCategory, setIsCategory } =
+    React.useContext(CategoriesContext);
+  const {
+    productOverview,
+    setProductOverview,
+    scroll,
+    setScroll,
+    inputScroll,
+    setInputScroll,
+  } = React.useContext(ProductContext);
+
+  useEffect(() => {
+    if (!scroll) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else {
+      if (scroll) {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }
+      if (inputScroll) {
+        window.scrollTo({
+          top: 600,
+          behavior: "smooth",
+        });
+      }
+      setScroll(false);
+    }
+  }, [isCategory, productOverview]);
+
   return (
-    <ContainerRightInternal >
-       <Titles titles={isCategory?.name} />
-       <ListProducts type={'isCategory'} products={isCategory?.products}/>
+    <ContainerRightInternal>
+      <Titles titles={isCategory?.name} isHome={false} />
+      {isCategory?.products?.length === 0 ? (
+        <IsResul>
+          <Success message={"Sem Resultado"} icon={isResult} isResult={true} />
+        </IsResul>
+      ) : (
+        <ListProducts
+          type={"isCategory"}
+          products={isCategory?.products}
+          category={isCategory}
+        />
+      )}
     </ContainerRightInternal>
-  )
-}
+  );
+};
+
+export const IsResul = styled.div`
+  width: 100%;
+  height: 100%;
+`;
