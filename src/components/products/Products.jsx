d7b90ac29c4deec50a47ca56b99679cacfb4b04b
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-02 22:36:44 -0300

    :sparkles:	feat:removing bug of price that did not appear and blocking user not logged in

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -7,13 +7,19 @@ import {
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import banner from "../../assets/images/ipad-card-40-pro-202108 1.png";
-import { Checkbox } from "@mui/material";
+import { Box, Checkbox, Grid } from "@mui/material";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 import { ProductContext } from "../../contexts/ProductContext";
 import { useCreateFavorites } from "../../hooks/UseFavorites";
 import { FavoritesContext } from "../../contexts/FavoritesContext";
+import { CategoriesContext } from "../../contexts/Categories";
+import {
+  getFromLocalStorage,
+  saveToLocalStorage,
+} from "../../localStorage/LocalStorage";
+import StarIcon from "@mui/icons-material/Star";
 export const Products = ({
   name,
   avaliações,
@@ -22,6 +28,7 @@ export const Products = ({
   quantity,
   id,
   isFavorite,
+  category,
 }) => {
   const navigate = useNavigate();
   const [checked, setChecked] = useState(false);
@@ -31,7 +38,7 @@ export const Products = ({
     React.useContext(ProductContext);
   const { myFavorities, setMyFavorities, favorities } =
     React.useContext(FavoritesContext);
-
+  const { setIsCategory } = React.useContext(CategoriesContext);
   useEffect(() => {
     if (favorities?.filter((f) => f.id === id).length > 0 || isFavorite) {
       setChecked(true);
@@ -40,7 +47,15 @@ export const Products = ({
     }
   }, [favorities, id, isFavorite]);
 
-  function showProduct(name, avaliações, image, description, quantity, id) {
+  function showProduct(
+    name,
+    avaliações,
+    image,
+    description,
+    quantity,
+    id,
+    category
+  ) {
     setProductOverview({
       name,
       price: avaliações,
@@ -48,7 +63,9 @@ export const Products = ({
       description,
       quantity,
       id,
+      category,
     });
+    setIsCategory(category);
     navigate("/product");
   }
   const { createFavorites, removeFavorites } = useCreateFavorites();
@@ -58,6 +75,13 @@ export const Products = ({
     if (!checked) {
       const hasError = await createFavorites(id);
       setMyFavorities(id);
+      if (hasError) {
+        setChecked(false);
+        toast.success("Faça Login Primeiro!", {
+          className: "custom-toast",
+        });
+      }
+     
     } else {
       const hasError = await removeFavorites(id);
     }
@@ -65,15 +89,18 @@ export const Products = ({
 
   return (
     <ProductsContainer>
+    
       <ImageContainer
-        onClick={() => showProduct(name, avaliações, image, description, 1, id)}
+        onClick={() =>
+          showProduct(name, avaliações, image, description, 1, id, category)
+        }
       >
         <img src={image} />
       </ImageContainer>
       <InformationProducts>
         <h1
           onClick={() =>
-            showProduct(name, avaliações, image, description, 1, id)
+            showProduct(name, avaliações, image, description, 1, id, category)
           }
         >
           {name}
@@ -81,14 +108,17 @@ export const Products = ({
 
         <p
           onClick={() =>
-            showProduct(name, avaliações, image, description, 1, id)
+            showProduct(name, avaliações, image, description, 1, id, category)
           }
         >
           {" "}
-          {avaliações}
+          R$ {avaliações}
         </p>
         <div className="containerProductsLow">
-          Avaliações
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <StarIcon style={{ color: "#FB8500" }} />
+            <p>4.9</p>
+          </Box>
           <div>
             <Checkbox
               {...label}
