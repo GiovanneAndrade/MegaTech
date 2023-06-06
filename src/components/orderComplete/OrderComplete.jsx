b61067501a484184ecb0e76faeeda822cf6e4c23
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-06 18:58:24 -0300

    :sparkles:	feat:  add responsive order component

--- src/components/orderComplete/OrderComplete.jsx
+++ src/components/orderComplete/OrderComplete.jsx
@@ -7,17 +7,18 @@ import CancelOrder from "./CancelOrder";
 import { OrderContext } from "../../contexts/OrderContext";
 import Lottie from "react-lottie";
 import animationData from "../../assets/images/data.json";
+import { H1 } from "../EditProfile/UserProfileComponents/ControllerNotifications";
+import { useNavigate } from "react-router-dom";
 
 const OrderComplete = () => {
   const deliveryDate = addDays(new Date(), 15);
   const formattedDeliveryDate = format(deliveryDate, "dd/MM/yyyy");
   const { finalOrder, setFinalOrder } = React.useContext(OrderContext);
-
   const total = finalOrder?.map((f) => f?.total);
   const address = finalOrder?.filter((f) => f?.address);
   const card = finalOrder?.filter((f) => f?.card);
   const installmentNumber = finalOrder?.filter((f) => f?.installmentNumber);
-
+  const navigate = useNavigate();
   const defaultOptions = {
     loop: false,
     autoplay: true,
@@ -31,13 +32,13 @@ const OrderComplete = () => {
     width: "100px",
     height: "100px",
   };
+
   return (
     <OrderCompleteContainer>
       <div className="container">
         <Grid
           container
           direction="column"
-          spacing={2}
           style={{
             background: "#f7f7f7",
             marginTop: "45px",
@@ -59,7 +60,9 @@ const OrderComplete = () => {
             </Grid>
             <Grid item container justify="space-between" alignItems="center">
               <Grid item>
-                <Typography variant="body1">Total do Pedido:</Typography>
+                <Typography variant="body1">
+                  <H1>Total do Pedido:</H1>
+                </Typography>
               </Grid>
 
               <Grid item>
@@ -67,7 +70,9 @@ const OrderComplete = () => {
               </Grid>
             </Grid>
             <Grid item>
-              <Typography variant="body1">Entregar em:</Typography>
+              <Typography variant="body1">
+                <H1>Entregar em:</H1>
+              </Typography>
               <Typography variant="body2">
                 {" "}
                 <strong>endereço:</strong>
@@ -78,34 +83,48 @@ const OrderComplete = () => {
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant="body1">Entrega até:</Typography>
+              <Typography variant="body1">
+                <H1>Entrega até:</H1>
+              </Typography>
               <Typography variant="body2">{formattedDeliveryDate}</Typography>
             </Grid>
             <Grid item>
-              <Typography variant="body1">Cartão utilizado:</Typography>
+              <Typography variant="body1">
+                <H1>Cartão utilizado:</H1>
+              </Typography>
               <Typography variant="body2">
                 credito **** {card[0]?.card?.last4Digits}
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant="body1">Parcelas:</Typography>
+              <Typography variant="body1">
+                <H1>Parcelas:</H1>
+              </Typography>
               <Typography variant="body2">
                 {installmentNumber[0]?.installmentNumber}x
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant="body1">Numero do Pedido:</Typography>
+              <Typography variant="body1">
+                <H1>Numero do Pedido:</H1>
+              </Typography>
               <Typography variant="body2">156198419815</Typography>
             </Grid>
             <Grid
               item
-              style={{ display: "flex", justifyContent: "space-between" }}
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: "0",
+                marginTop: "10px",
+              }}
             >
               <CancelOrder />
               <Button
                 variant="contained"
                 color="primary"
-                style={{ marginRight: "20px", width: "400px" }}
+                className="buttonHome"
+                onClick={() => navigate("/")}
               >
                 Voltar para a Loja
               </Button>
@@ -122,8 +141,33 @@ const OrderCompleteContainer = styled.div`
   width: 100%;
   align-items: center;
   justify-content: center;
+  Button {
+    margin-right: 10px;
+    height: 40px;
+    width: 200px;
+    padding: 10px;
+  }
+  .buttonHome {
+    width: 250px;
+  }
   div {
     width: 500px;
+  }
+  @media screen and (max-width: 768px) {
+    padding: 10px;
+    div {
+      width: 100%;
+    }
+    Button {
+      margin-right: 10px;
+      height: 40px;
+      width: 150px;
+      font-size: 10px;
+      margin-right: 0;
+    }
+    .buttonHome {
+      width: 250px;
+    }
   }
 `;
 
