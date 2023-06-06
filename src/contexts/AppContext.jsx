783f8cf0cb2f59e2443d19f1fa9a1a29b43743b4
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-05 23:12:20 -0300

    :sparkles:	feat:  logic of selecting the cards registered in the context

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {  useState } from "react";
 
 export const AppContext = React.createContext({});
-const teste = "teste";
+ 
 export const AppProvider = (props) => {
+ const [ newCard, setNewCard] = useState([{ name: "Selecione um cartão" }])
+ const [ showCard, setShowCard ] = useState( null)
+ const [ saved , setSaved ] = useState(false)
   return (
-    <AppContext.Provider value={{ teste }}>
+    <AppContext.Provider value={{  newCard, setNewCard,  showCard, setShowCard , saved , setSaved}}>
       {props.children}
     </AppContext.Provider>
   );
