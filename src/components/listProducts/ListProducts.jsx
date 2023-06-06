9ccf35854cc9c54872952ce6666407d2d5044fb4
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-05 23:22:03 -0300

    :sparkles:	fix:  cleaning unnecessary comments from ListProdcuts component

--- src/components/listProducts/ListProducts.jsx
+++ src/components/listProducts/ListProducts.jsx
@@ -10,10 +10,10 @@ import banner from "../../assets/images/ipad-card-40-pro-202108 1.png";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { GrPrevious, GrNext } from "react-icons/gr";
-export const ListProducts = ({ products, type }) => {
+export const ListProducts = ({ products, type, category }) => {
   const [prevBgColor, setPrevBgColor] = useState("#F6AE2D");
   const [nextBgColor, setNextBgColor] = useState("#F6AE2D");
- 
+
   const prev = () => {
     setNextBgColor("#F6AE2D");
   };
@@ -31,16 +31,6 @@ export const ListProducts = ({ products, type }) => {
   return (
     <HomeContainer>
       <ListProductsContainer type={type}>
-        {/*  <Next
-          onClick={() =>  toast("Pra diretia",{ theme: "dark" })}
-          
-          prevBgColor={prevBgColor}
-          onMouseEnter={handleNextMouseLeave}
-          onMouseLeave={handlePrevMouseLeave}
-        >
-          <ToastContainer />
-          <GrPrevious style={{ display: `${prevArrow}` }} />
-        </Next> */}
         {products?.map((product) => (
           <Products
             name={product.name}
@@ -49,16 +39,9 @@ export const ListProducts = ({ products, type }) => {
             description={product.description}
             quantity={product?.quantity}
             id={product?.id}
+            category={category}
           />
         ))}
-        {/*  <Prev
-          onClick={() => toast("Pra diretia",{ theme: "dark" })}
-          nextBgColor={nextBgColor}
-          onMouseEnter={prev}
-          onMouseLeave={next}
-        >
-          <GrNext style={{ display: `${nextArrow}` }} />
-        </Prev> */}
       </ListProductsContainer>
     </HomeContainer>
   );
