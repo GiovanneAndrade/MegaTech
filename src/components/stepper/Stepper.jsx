babc41b9b34fd795026ebbf3f6c040521d5adb3f
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-06 19:14:05 -0300

    :sparkles:	fix:  leaving dynamic stepper component

--- src/components/stepper/Stepper.jsx
+++ src/components/stepper/Stepper.jsx
@@ -10,11 +10,22 @@ import { Amount, StepperContainer } from "./StepperStyles";
 import { AddAnddress } from "../address/Address";
 import AddressMap from "../address/AddressMap";
 import CreditCard from "../creditCard/CreditCard";
-import { AddAnddressContainer, AddAnddressSummary } from "../address/AddressStyles";
+import {
+  AddAnddressContainer,
+  AddAnddressSummary,
+} from "../address/AddressStyles";
 import OrderSummary from "../summary/Summary";
 import OrderComplete from "../orderComplete/OrderComplete";
 import CancelOrder from "../orderComplete/CancelOrder";
-
+import { getFromLocalStorage } from "../../localStorage/LocalStorage";
+import { OrderContext } from "../../contexts/OrderContext";
+import { AnddressContext } from "../../contexts/Anddress";
+import { AppContext } from "../../contexts/AppContext";
+import { useCreateOrder } from "../../hooks/UseOrders";
+import { Saved } from "../creditCard/Saved";
+import pare from "../../assets/images/pare.gif";
+import { NewContainerHome } from "../../pages/historic/Historic";
+import { ContainerHome } from "../../pages/home/Home";
 const steps = [
   "Confirme os produtos",
   "Escolha o endereço",
@@ -24,7 +35,23 @@ const steps = [
 export default function HorizontalLinearStepper() {
   const [activeStep, setActiveStep] = React.useState(0);
   const [skipped, setSkipped] = React.useState(new Set());
-  console.log(activeStep);
+  const [productSelection, setProductSelection] = React.useState();
+  const { selectedAddress, setSelectedAddress } =
+    React.useContext(AnddressContext);
+  const {
+    isTotal,
+    setIsTotal,
+    finalOrder,
+    errorOrder,
+    setErrorOrder,
+    setNewOrder,
+    newOrder,
+  } = React.useContext(OrderContext);
+  const { newCard, setNewCard, showCard, setShowCard, saved, setSaved } =
+    React.useContext(AppContext);
+
+  const { postOrder } = useCreateOrder();
+
   const isStepOptional = (step) => {
     return step === 1;
   };
@@ -33,25 +60,43 @@ export default function HorizontalLinearStepper() {
     return skipped.has(step);
   };
 
-  const handleNext = () => {
+  const handleNext = async (activeStep, finalOrder, postOrder) => {
+    if (!isTotal || isTotal.length === 0) return alert("Escolha um produto");
     let newSkipped = skipped;
     if (isStepSkipped(activeStep)) {
       newSkipped = new Set(newSkipped.values());
       newSkipped.delete(activeStep);
     }
-
+    if (activeStep === 1 && !selectedAddress) {
+      return alert("Escolha o Endereço");
+    }
+    if (activeStep === 2 && !showCard) {
+      return alert("Adicione ou Escolha o Cartão de Pagamento");
+    }
+    if (activeStep === 2) {
+      const hasError = await postOrder(finalOrder);
+      if (hasError) {
+        return;
+      }
+    }
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
     setSkipped(newSkipped);
   };
 
-  const handleBack = () => {
+  const handleBack = (activeStep) => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+    if (activeStep === 1) {
+      localStorage.removeItem("selectedProducts");
+      setIsTotal([]);
+    }
+    if (activeStep === 2) {
+      setSelectedAddress(null);
+    }
   };
 
   const handleSkip = () => {
     if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
       throw new Error("You can't skip a step that isn't optional.");
     }
 
@@ -67,9 +112,18 @@ export default function HorizontalLinearStepper() {
     setActiveStep(0);
   };
 
+  const megaTechCart = getFromLocalStorage("megaTechCart");
+  const existingProducts =
+    JSON.parse(localStorage.getItem("selectedProducts")) || [];
+  const total = isTotal?.reduce(
+    (acc, item) => acc + Number(item.price) * Number(item.quantity),
+    0
+  );
+
+  const myToken = getFromLocalStorage("megaTechAuth");
   return (
-    <Box sx={{ width: "100%" }}>
-      <Stepper activeStep={activeStep}>
+    <Box sx={{ width: "100%"}}>
+      <Stepper activeStep={activeStep} sx={{paddingBottom:'20px' }}>
         {steps.map((label, index) => {
           const stepProps = {};
           const labelProps = {};
@@ -91,8 +145,7 @@ export default function HorizontalLinearStepper() {
       {activeStep === steps.length ? (
         <React.Fragment>
           <Typography sx={{ mt: 2, mb: 1 }}>
-           <OrderComplete/>
-           
+            <OrderComplete />
           </Typography>
           <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
             <Box sx={{ flex: "1 1 auto" }} />
@@ -104,21 +157,38 @@ export default function HorizontalLinearStepper() {
           <Typography sx={{ mt: 2, mb: 1 }}>
             {activeStep === 0 ? (
               <StepperContainer>
-                <CloseOrder />
-                <CloseOrder />
-                <CloseOrder />
+                {megaTechCart?.map((i) => (
+                  <CloseOrder
+                    id={i?.id}
+                    img={i?.image}
+                    quantitys={i?.quantity}
+                    price={i?.price}
+                    name={i?.name}
+                    productSelection={productSelection}
+                  />
+                ))}
+
                 <Amount>
-                  <p>valor total R$ 1500</p>
+                  <p>valor total R$ {total ? total : "0"}</p>
                 </Amount>
               </StepperContainer>
             ) : activeStep + 1 === 2 ? (
-              <AddAnddress />
+              myToken ? (
+                <AddAnddress />
+              ) : (
+                <ContainerHome>
+                  <NewContainerHome>
+                    Faça Login Para Prosseguir
+                    <img src={pare} />
+                  </NewContainerHome>
+                </ContainerHome>
+              )
             ) : (
               <AddAnddressContainer>
                 <AddAnddressSummary>
-                  <CreditCard />
+                  {saved ? <Saved /> : <CreditCard />}
                 </AddAnddressSummary>
-                <OrderSummary/>
+                <OrderSummary isTotal={total} />
               </AddAnddressContainer>
             )}
           </Typography>
@@ -127,7 +197,7 @@ export default function HorizontalLinearStepper() {
             <Button
               color="inherit"
               disabled={activeStep === 0}
-              onClick={handleBack}
+              onClick={() => handleBack(activeStep)}
               sx={{ mr: 1 }}
             >
               Back
@@ -139,7 +209,9 @@ export default function HorizontalLinearStepper() {
               </Button>
             )} */}
 
-            <Button onClick={handleNext}>
+            <Button
+              onClick={() => handleNext(activeStep, finalOrder, postOrder)}
+            >
               {activeStep === steps.length - 1 ? "Finish" : "Next"}
             </Button>
           </Box>
