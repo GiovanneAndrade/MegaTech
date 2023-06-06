776bb2453b46f9da9d2140ac0f091afe3d369145
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-06 18:55:44 -0300

    :sparkles:	fix:  fixing layout and adding responsiveness in card componet

--- src/components/creditCard/CreditCard.jsx
+++ src/components/creditCard/CreditCard.jsx
@@ -4,38 +4,6 @@ import "react-credit-cards/es/styles-compiled.css";
 import styled from "styled-components";
 import { Buttons } from "../button/Button";
 
-const CreditCardWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const FormWrapper = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 20px;
-`;
-
-const Label = styled.label`
-  font-size: 16px;
-  margin-top: 10px;
-`;
-
-const Input = styled.input`
-  border: none;
-  border-bottom: 1px solid #d6d6d6;
-  font-size: 16px;
-  margin-bottom: 20px;
-  padding: 10px;
-  width: 100%;
-
-  &:focus {
-    border-bottom: 1px solid #000;
-    outline: none;
-  }
-`;
-
 function CreditCard() {
   const [cardData, setCardData] = useState({
     cvc: "",
@@ -45,7 +13,7 @@ function CreditCard() {
     number: "",
     cardUser: "",
   });
- console.log(cardData.number)
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setCardData((prevState) => ({
@@ -126,7 +94,7 @@ function CreditCard() {
             }))
           }
         />
-        <Input
+        {/*  <Input
           type="text"
           name="Apelido"
           maxLength="20"
@@ -139,11 +107,51 @@ function CreditCard() {
               focus: event.target.name,
             }))
           }
+        /> */}
+        <Buttons
+          text={"Salvar Cartão"}
+          cardData={cardData}
+          typeButton={null}
+          variant={"contained"}
         />
-        <Buttons text={"Salvar Cartão"} cardData={cardData} typeButton={null} />
       </FormWrapper>
     </CreditCardWrapper>
   );
 }
 
 export default CreditCard;
+
+const CreditCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media screen and (max-width: 768px) {
+    min-width: 100%;
+  }
+`;
+
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+const Label = styled.label`
+  font-size: 16px;
+  margin-top: 10px;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid #d6d6d6;
+  font-size: 16px;
+  margin-bottom: 20px;
+  padding: 10px;
+  width: 60%;
+  text-align: center;
+  &:focus {
+    border-bottom: 1px solid #000;
+    outline: none;
+  }
+`;
