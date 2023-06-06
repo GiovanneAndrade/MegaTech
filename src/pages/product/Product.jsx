db955a9a42d33b146d0226f5e9aa5726f4c84c96
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-05 23:03:55 -0300

    :sparkles:	feat:  tweaks to product page componet positioning

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Nav } from "../../components/nav/Nav";
-import { ProductOverview } from "../../components/ProductOverview/ProductOverview";
+import ProductOverview from "../../components/ProductOverview/ProductOverview";
 import { CategoriesContext } from "../../contexts/Categories";
 import { ShowCategory } from "../categories/ShowCategory";
+import Teste, { ProductContainer } from "../../../teste/Teste";
+import { ListProducts } from "../../components/listProducts/ListProducts";
+import { Titles } from "../../components/title/Title";
+import {
+  ContainerProductList,
+  ProductOverviewContainer,
+  ProductOverviewShowCategory,
+} from "../../components/ProductOverview/ProductOverviewStyles";
+import styled from "styled-components";
+import { ProductContext } from "../../contexts/ProductContext";
 
 export const Product = () => {
-  const { category, showCategory, setShowCategory } =
-    React.useContext(CategoriesContext);
   return (
     <>
       <Nav />
       <ProductOverview />
+      <ProductOverviewContainer>
+        <ProductOverviewContainerBotton>
+          <ProductOverviewShowCategory>
+            <ShowCategory />
+          </ProductOverviewShowCategory>
+        </ProductOverviewContainerBotton>
+      </ProductOverviewContainer>
     </>
   );
 };
+
+export const ProductOverviewContainerBotton = styled(ContainerProductList)`
+  background: transparent;
+`;
