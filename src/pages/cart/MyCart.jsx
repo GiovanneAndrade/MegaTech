537b8a1db2353913f6c689311e83b0026cc98ff4
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-06 19:04:40 -0300

    :sparkles:	feat: add responsiveness on cart page

--- src/pages/cart/MyCart.jsx
+++ src/pages/cart/MyCart.jsx
@@ -24,6 +24,7 @@ import styled from "styled-components";
 import { Buttons } from "../../components/button/Button";
 import { useNavigate } from "react-router-dom";
 import useShoppingCart from "../../hooks/UseCart";
+import { Box } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -55,37 +56,55 @@ function MyCart() {
         <CardHeader title="Carrinho" className={classes.cardHeader} />
         <Divider />
         <CardContent>
-          <List>
+          <NewList>
             {items.map((item) => (
-              <ListItem key={item.id}>
-                <ListItemAvatar>
-                  <img
-                    src={item.image}
-                    alt={item.name}
-                    className={classes.productImage}
-                  />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={item.name}
-                  secondary={`$${item.price}`}
-                />
+              <NewListItem key={item.id}>
+                <LeftBox>
+                  <ListItemAvatar>
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      className={classes.productImage}
+                    />
+                  </ListItemAvatar>
+                  <Box>
+                    <ListItemText
+                      primary={item.name}
+                      secondary={`$${item.price}`}
+                    />
+                    <ResponsiveBox>
+                      quantidade
+                      <IconButton onClick={() => handleRemoveItem(item.id)}>
+                        <RemoveIcon />
+                      </IconButton>
+                      {item.quantity}
+                      <IconButton onClick={() => handleAddItem(item.id)}>
+                        <AddIcon />
+                      </IconButton>
+                    </ResponsiveBox>
+                  </Box>
+                </LeftBox>
+
                 <NewListItemSecondaryAction>
-                  <IconButton onClick={() => handleRemoveItem(item.id)}>
-                    <RemoveIcon />
-                  </IconButton>
-                  {item.quantity}
-                  <NewTypography> 
+                  <WebBox>
+                    <IconButton onClick={() => handleRemoveItem(item.id)}>
+                      <RemoveIcon />
+                    </IconButton>
+                    {item.quantity}
+
                     <IconButton onClick={() => handleAddItem(item.id)}>
                       <AddIcon />
                     </IconButton>
+                  </WebBox>
+                  <DeleteBox>
                     <IconButton onClick={() => handleDeleteItem(item.id)}>
-                      <DeleteIcon />
+                      <DeleteIcon className="icon" />
                     </IconButton>
-                  </NewTypography>
+                  </DeleteBox>
                 </NewListItemSecondaryAction>
-              </ListItem>
+              </NewListItem>
             ))}
-          </List>
+          </NewList>
           {items.length === 0 ? (
             <Typography align="center">Your cart is empty</Typography>
           ) : (
@@ -112,10 +131,51 @@ export default MyCart;
 export const RootCard = styled(Card)`
   min-width: 50%;
 `;
+export const LeftBox = styled(Box)`
+  display: flex;
+`;
+
+export const WebBox = styled(Box)`
+  display: flex;
+  justify-content: center;
+
+  align-items: center;
+  // background-color: #000;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+export const DeleteBox = styled(Box)`
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    justify-content: right;
+    text-align: end;
+    .icon {
+      margin-right: -22px;
+    }
+  }
+`;
+export const ResponsiveBox = styled(Box)`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: relative;
+    z-index: 1000;
+  }
+`;
+
 export const NewListItemSecondaryAction = styled(ListItemSecondaryAction)`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   gap: 15px;
+  height: 100%;
+  width: 50%;
+  //background-color: #000;
 `;
 export const NewTypography = styled(Typography)`
   display: flex;
@@ -125,4 +185,22 @@ export const FinalTypography = styled(Typography)`
   display: flex;
   justify-content: space-between;
   padding-top: 20px;
+`;
+export const NewList = styled(List)`
+  display: flex;
+  flex-direction: column;
+
+  gap: 15px;
+`;
+export const NewListItem = styled(ListItem)`
+  @media screen and (max-width: 768px) {
+    height: 120px;
+    padding: 10px;
+    padding-right: 0;
+    font-size: 13px;
+    gap: 5px;
+  /*   background: #eee;
+    box-shadow: 0px 10px 40px rgba(0, 0, 0, 0.1);
+    border-radius: 4px; */
+  }
 `;
