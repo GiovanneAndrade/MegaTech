0c487ad611bde0ea7c118de701c1d1680ecac959
GiovanneAndrade <vanne.as@hotmail.com>
2023-06-12 12:30:13 -0300

    :sparkles:	feat:  add token state

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getFromLocalStorage } from "../localStorage/LocalStorage";
 
 export const UserContext = React.createContext({});
-const User = "User";
+ 
 export const UserProvider = (props) => {
+  const [ token, setToken ] = useState()
+  const [showOtherComponent, setShowOtherComponent] = useState("editProfile");
+  useEffect(() => {
+    // Verifica se há um token no local storage
+    const tokenLocal = getFromLocalStorage('megaTechAuth')
+
+    // Se houver um token no local storage, atualiza o estado com o valor do token
+    if (tokenLocal) {
+      setToken(tokenLocal);
+    }
+  }, []);
   return (
-    <UserContext.Provider value={{ User }}>
+    <UserContext.Provider value={{ token, setToken, showOtherComponent, setShowOtherComponent }}>
       {props.children}
     </UserContext.Provider>
   );
