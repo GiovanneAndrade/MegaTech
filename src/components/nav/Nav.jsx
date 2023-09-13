4bccb8a48c96eac78936226250d74c698cb090fe
GiovanneAndrade <vanne.as@hotmail.com>
2023-09-13 20:35:02 -0300

    :sparkles:	feat: adding color to the menu of selected pages

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Input } from "../input/Input";
 import ShoppingCartSharpIcon from "@mui/icons-material/ShoppingCartSharp";
 import {
-  Logo,
-  Menu,
-  MenuCentral,
-  MenuItem,
-  MenuLeft,
-  MenuList,
-  NavContainer,
-  NewMenuCentral,
+    Logo,
+    Menu,
+    MenuCentral,
+    MenuItem,
+    MenuLeft,
+    MenuList,
+    NavContainer,
+    NewMenuCentral,
 } from "./NavStyles";
 import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
@@ -28,147 +28,197 @@ import LogoutIcon from "@mui/icons-material/Logout";
 import Tooltip from "@mui/material/Tooltip";
 import IconButton from "@mui/material/IconButton";
 import {
-  getFromLocalStorage,
-  removeFromLocalStorage,
+    getFromLocalStorage,
+    removeFromLocalStorage,
 } from "../../localStorage/LocalStorage";
 import { UserContext } from "../../contexts/UserContext";
+import { useLocation } from "react-router-dom";
 export const Nav = () => {
-  const [notifications, setNotifications] = React.useState(1);
-  const { showOtherComponent, setShowOtherComponent } = React.useContext(UserContext);
-  const { showCategory, setShowCategory } = React.useContext(CategoriesContext);
-  const navigate = useNavigate();
-  function home() {
-    navigate("/");
-    setShowCategory(true);
-  }
-  function userMessage() {
-    navigate("/user") 
-    setShowOtherComponent("message")
-  }
-  function pedidos() {
-    if (newOrder) {
-      setNewOrder(false);
-    } else {
-      setNewOrder(true);
+    const [notifications, setNotifications] = React.useState(1);
+    const { showOtherComponent, setShowOtherComponent } =
+        React.useContext(UserContext);
+    const { showCategory, setShowCategory } =
+        React.useContext(CategoriesContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const isCurrentPage = (pageName) => {
+        return location.pathname === pageName;
+    };
+
+    function home() {
+        navigate("/");
+        setShowCategory(true);
     }
-  }
+    function userMessage() {
+        navigate("/user");
+        setShowOtherComponent("message");
+    }
+    function pedidos() {
+        if (newOrder) {
+            setNewOrder(false);
+        } else {
+            setNewOrder(true);
+        }
+    }
 
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
 
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  const lottieStyle = {
-    width: "50px",
-    height: "50px",
-    margin: "0",
-  };
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
+    const lottieStyle = {
+        width: "50px",
+        height: "50px",
+        margin: "0",
+    };
 
-  const myToken = getFromLocalStorage("megaTechAuth");
+    const myToken = getFromLocalStorage("megaTechAuth");
 
-  function deleteToken() {
-    removeFromLocalStorage("megaTechAuth");
-    setTimeout(() => {
-      navigate("/");
-      window.location.reload();
-    }, 1000);
-  }
-  return (
-    <NavContainer myToken={myToken}>
-      <MenuList>
-        <MenuLeft >
-          <Logo src={logo} onClick={home} myToken={myToken}/>
+    function deleteToken() {
+        removeFromLocalStorage("megaTechAuth");
+        setTimeout(() => {
+            navigate("/");
+            window.location.reload();
+        }, 1000);
+    }
+    return (
+        <NavContainer myToken={myToken}>
+            <MenuList>
+                <MenuLeft>
+                    <Logo src={logo} onClick={home} myToken={myToken} />
 
-          <div  >
-            <h1>CEP</h1> <Lottie options={defaultOptions} style={lottieStyle} />
-          </div>
-        </MenuLeft>
-        <MenuCentral>
-          <Input />
+                    <div>
+                        <h1>CEP</h1>{" "}
+                        <Lottie options={defaultOptions} style={lottieStyle} />
+                    </div>
+                </MenuLeft>
+                <MenuCentral>
+                    <Input />
 
-          <ul>
-            <li>
-              <Categories />
-            </li>
-            <li onClick={() => navigate("/offers")}>Ofertas</li>
-            <li onClick={() => navigate("/historic")}>Historico</li>
-            <li onClick={() => navigate("/favorites")}>Favoritos</li>
-            <li onClick={() => navigate("/contact")}>Contato</li>
-            <li onClick={() => navigate("/orders")}>meus pedidos</li>
-          </ul>
-        </MenuCentral>
-        <Menu>
-          {myToken ? (
-            <div>
-              <Tooltip title="Finalizar sessão">
-                <IconButton>
-                  <LogoutIcon
-                    sx={{
-                      color: "#f6ae2d",
-                      cursor: "pointer",
-                      marginRight: "9px",
-                    }}
-                    onClick={deleteToken}
-                  />
-                </IconButton>
-              </Tooltip>
+                    <ul>
+                        <li>
+                            <Categories />
+                        </li>
+                        <li
+                            className={isCurrentPage("/offers") ? "active" : ""}
+                            onClick={() => navigate("/offers")}
+                        >
+                            Ofertas
+                        </li>
+                        <li
+                            className={
+                                isCurrentPage("/historic") ? "active" : ""
+                            }
+                            onClick={() => navigate("/historic")}
+                        >
+                            Historico
+                        </li>
+                        <li
+                            className={
+                                isCurrentPage("/favorites") ? "active" : ""
+                            }
+                            onClick={() => navigate("/favorites")}
+                        >
+                            Favoritos
+                        </li>
+                        <li
+                            className={
+                                isCurrentPage("/contact") ? "active" : ""
+                            }
+                            onClick={() => navigate("/contact")}
+                        >
+                            Contato
+                        </li>
+                        <li
+                            className={isCurrentPage("/orders") ? "active" : ""}
+                            onClick={() => navigate("/orders")}
+                        >
+                            meus pedidos
+                        </li>
+                    </ul>
+                </MenuCentral>
+                <Menu>
+                    {myToken ? (
+                        <div>
+                            <Tooltip title="Finalizar sessão">
+                                <IconButton>
+                                    <LogoutIcon
+                                        sx={{
+                                            color: "#f6ae2d",
+                                            cursor: "pointer",
+                                            marginRight: "9px",
+                                        }}
+                                        onClick={deleteToken}
+                                    />
+                                </IconButton>
+                            </Tooltip>
 
-              <Badge
-                badgeContent={notifications}
-                color="error"
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-              >
-                <NotificationsActiveIcon
-                 onClick={userMessage}
-                  sx={{
-                    color: "#f6ae2d",
-                    cursor: "pointer",
-                    marginRight: "9px",
-                  }}
-                />
-              </Badge>
-              <Tooltip title="Gerencie as informações do seu perfil aqui">
-                <IconButton>
-                  <AccountCircleIcon
-                    onClick={() => navigate("/user")}
-                    sx={{ color: "#f6ae2d", cursor: "pointer" }}
-                  />
-                </IconButton>
-              </Tooltip>
-              <Cart />
-            </div>
-          ) :   <Cart />}
+                            <Badge
+                                badgeContent={notifications}
+                                color="error"
+                                anchorOrigin={{
+                                    vertical: "bottom",
+                                    horizontal: "left",
+                                }}
+                            >
+                                <NotificationsActiveIcon
+                                    onClick={userMessage}
+                                    sx={{
+                                        color: "#f6ae2d",
+                                        cursor: "pointer",
+                                        marginRight: "9px",
+                                    }}
+                                />
+                            </Badge>
+                            <Tooltip title="Gerencie as informações do seu perfil aqui">
+                                <IconButton>
+                                    <AccountCircleIcon
+                                        onClick={() => navigate("/user")}
+                                        sx={{
+                                            color: "#f6ae2d",
+                                            cursor: "pointer",
+                                        }}
+                                    />
+                                </IconButton>
+                            </Tooltip>
+                            <Cart />
+                        </div>
+                    ) : (
+                        <Cart />
+                    )}
 
-          {!myToken ? (
-            <ul>
-              <MenuItem onClick={() => navigate("/signup")}>
-                Crie Sua Conta
-              </MenuItem>
-              <MenuItem onClick={() => navigate("/signin")}>Entre</MenuItem>
-            </ul>
-          ) : null}
-        </Menu>
-        <NewMenuCentral>
-          <Input />
-          <ul>
-          <li>
-              <Categories />
-            </li>
-            <li onClick={() => navigate("/offers")}>Ofertas</li>
-            <li onClick={() => navigate("/historic")}>Historico</li>
-            <li onClick={() => navigate("/favorites")}>Favoritos</li>
-            <li onClick={() => navigate("/contact")}>Contato</li>
-            <li onClick={() => navigate("/orders")}>pedidos</li>
-          </ul>
-        </NewMenuCentral>
-      </MenuList>
-    </NavContainer>
-  );
+                    {!myToken ? (
+                        <ul>
+                            <MenuItem onClick={() => navigate("/signup")}>
+                                Crie Sua Conta
+                            </MenuItem>
+                            <MenuItem onClick={() => navigate("/signin")}>
+                                Entre
+                            </MenuItem>
+                        </ul>
+                    ) : null}
+                </Menu>
+                <NewMenuCentral>
+                    <Input />
+                    <ul>
+                        <li>
+                            <Categories />
+                        </li>
+                        <li onClick={() => navigate("/offers")}>Ofertas</li>
+                        <li onClick={() => navigate("/historic")}>Historico</li>
+                        <li onClick={() => navigate("/favorites")}>
+                            Favoritos
+                        </li>
+                        <li onClick={() => navigate("/contact")}>Contato</li>
+                        <li onClick={() => navigate("/orders")}>pedidos</li>
+                    </ul>
+                </NewMenuCentral>
+            </MenuList>
+        </NavContainer>
+    );
 };
--- src/components/nav/NavStyles.jsx
+++ src/components/nav/NavStyles.jsx
@@ -1,143 +1,145 @@
 import styled from "styled-components";
 
 export const NavContainer = styled.nav`
-  width: 100%;
-  height: 117px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #32357b;
-  color: #fff;
-  padding: 1rem;
+    width: 100%;
+    height: 117px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #32357b;
+    color: #fff;
+    padding: 1rem;
 
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-    min-height: ${(props) => (props.myToken ? "155px" : "165px")};
-    padding-top: 1.5rem;
-  }
+    @media screen and (max-width: 768px) {
+        flex-direction: column;
+        min-height: ${(props) => (props.myToken ? "155px" : "165px")};
+        padding-top: 1.5rem;
+    }
 `;
 
 export const MenuLeft = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  div {
     display: flex;
-    align-items: center;
-    //justify-content: center;
-    cursor: pointer;
-    display: none;
-  }
-  @media screen and (max-width: 768px) {
-    margin-top: -20px;
-    gap: 0;
-    width: 100%;
-    font-size: 13px;
+    flex-direction: column;
 
-    h1 {
-      margin-top: 25px;
+    div {
+        display: flex;
+        align-items: center;
+        //justify-content: center;
+        cursor: pointer;
+        display: none;
     }
-  }
+    @media screen and (max-width: 768px) {
+        margin-top: -20px;
+        gap: 0;
+        width: 100%;
+        font-size: 13px;
+
+        h1 {
+            margin-top: 25px;
+        }
+    }
 `;
 
 export const MenuList = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  /* margin: 0 15rem; */
-  align-items: flex-end;
-
-  ul {
+    width: 100%;
     display: flex;
-    gap: 1rem;
-  }
-  li {
-    list-style: none;
-  }
+    justify-content: space-around;
+    /* margin: 0 15rem; */
+    align-items: flex-end;
 
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-    align-items: center;
-    margin-top: 1rem;
-    position: relative;
-    z-index: 0;
-  }
+    ul {
+        display: flex;
+        gap: 1rem;
+    }
+    li {
+        list-style: none;
+    }
+
+    @media screen and (max-width: 768px) {
+        flex-direction: column;
+        align-items: center;
+        margin-top: 1rem;
+        position: relative;
+        z-index: 0;
+    }
 `;
 
 export const MenuCentral = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    .active {
+        color: #f6ae2d;
+    }
+    li {
+        cursor: pointer;
+    }
 
-  li {
-    cursor: pointer;
-  }
-
-  @media screen and (max-width: 768px) {
-    margin-top: 1rem;
-    display: none;
-    position: relative;
-    z-index: 0;
-  }
+    @media screen and (max-width: 768px) {
+        margin-top: 1rem;
+        display: none;
+        position: relative;
+        z-index: 0;
+    }
 `;
 export const NewMenuCentral = styled(MenuCentral)`
-  display: none;
-  @media screen and (max-width: 768px) {
-    display: block;
-    width: 100%;
-    li {
-      font-size: 13px;
+    display: none;
+    @media screen and (max-width: 768px) {
+        display: block;
+        width: 100%;
+        li {
+            font-size: 13px;
+        }
     }
-  }
 `;
 
 export const Logo = styled.img`
-  width: 200px;
-  cursor: pointer;
-  @media screen and (max-width: 768px) {
-    margin-bottom: ${(props) => (props.myToken ? "1rem" : "0")};
-    width: 150px;
-    position: relative;
-    z-index: 1;
-  }
+    width: 200px;
+    cursor: pointer;
+    @media screen and (max-width: 768px) {
+        margin-bottom: ${(props) => (props.myToken ? "1rem" : "0")};
+        width: 150px;
+        position: relative;
+        z-index: 1;
+    }
 `;
 
 export const Menu = styled.ul`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  div {
     display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 0.5em;
-  }
-  ul {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    gap: 1rem;
-  }
-
-  @media screen and (max-width: 768px) {
-    align-items: end;
-    width: 100%;
-    margin-top: -55px;
-
-    ul {
-      margin-top: -20px;
-    }
+    flex-direction: column;
+    align-items: flex-end;
     div {
-      gap: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.5em;
     }
-  }
+    ul {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 1rem;
+    }
+
+    @media screen and (max-width: 768px) {
+        align-items: end;
+        width: 100%;
+        margin-top: -55px;
+
+        ul {
+            margin-top: -20px;
+        }
+        div {
+            gap: 0;
+        }
+    }
 `;
 
 export const MenuItem = styled.li`
-  //margin-left: 1rem;
-  cursor: pointer;
+    //margin-left: 1rem;
+    cursor: pointer;
 
-  @media screen and (max-width: 768px) {
-    margin-bottom: 1rem;
-  }
+    @media screen and (max-width: 768px) {
+        margin-bottom: 1rem;
+    }
 `;
