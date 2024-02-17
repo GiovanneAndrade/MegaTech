f9fc6979ce7589a41380eb6c9cffc91882973c6c
GiovanneAndrade <vanne.as@hotmail.com>
2024-02-16 21:06:23 -0300

    :sparkles:	feat: adding history route on main

--- src/main.jsx
+++ src/main.jsx
@@ -10,27 +10,30 @@ import { ProductProvider } from "./contexts/ProductContext";
 import { UserProvider } from "./contexts/UserContext";
 import { CategoriesProvider } from "./contexts/Categories";
 import { AnddressProvider } from "./contexts/Anddress";
+import {  HistoricProvider } from "./contexts/HistoricContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppProvider>
-      <CartProvider>
-        <FavoritesProvider>
-          <OrderProvider>
-            <PaymentProvider>
-              <ProductProvider>
-                <UserProvider>
-                  <AnddressProvider>
-                    <CategoriesProvider>
-                      <App />
-                    </CategoriesProvider>
-                  </AnddressProvider>
-                </UserProvider>
-              </ProductProvider>
-            </PaymentProvider>
-          </OrderProvider>
-        </FavoritesProvider>
-      </CartProvider>
-    </AppProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <AppProvider>
+            <CartProvider>
+                <FavoritesProvider>
+                    <OrderProvider>
+                        <PaymentProvider>
+                            <ProductProvider>
+                                <UserProvider>
+                                    <AnddressProvider>
+                                        <CategoriesProvider>
+                                            <HistoricProvider>
+                                                <App />
+                                            </HistoricProvider>
+                                        </CategoriesProvider>
+                                    </AnddressProvider>
+                                </UserProvider>
+                            </ProductProvider>
+                        </PaymentProvider>
+                    </OrderProvider>
+                </FavoritesProvider>
+            </CartProvider>
+        </AppProvider>
+    </React.StrictMode>
 );
